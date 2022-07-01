67d437a68c9f1216956ad281e3dfb53718f443f9
horamupa <horamupa@gmail.com>
2022-07-01 21:43:47 +0300

    Done Boss Challenge

--- KeyNote/ViewController.swift
+++ KeyNote/ViewController.swift
@@ -6,14 +6,50 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    
+    var player: AVAudioPlayer?
+    
+    
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(nani: sender.titleLabel?.text ?? " LOL")
+        sender.alpha = 0.5
 
+          //Code should execute after 0.2 second delay.
+          DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+              //Bring's sender's opacity back up to fully opaque.
+              sender.alpha = 1.0
+          }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    func playSound(nani: String) {
+        
+        let url = Bundle.main.url(forResource: nani, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player?.play()
+        
+    }
+//    func playSound() {
+//        guard let path = Bundle.main.path(forResource: "C", ofType:"wav") else {
+//            return }
+//        let url = URL(fileURLWithPath: path)
+//
+//        do {
+//            player = try AVAudioPlayer(contentsOf: url)
+//            player?.play()
+//
+//        } catch let error {
+//            print(error.localizedDescription)
+//        }
+//    }
+    
 }
 
